e2d38dd1c3cce60cd65d8f14a5d7d49d48584b4a
jooonak <ofnullable@gmail.com>
2020-03-28 21:14:54 +0900

    :lipstick: Update styles and add media.js, palettes.js for css in js variables

--- src/styles/media.js
+++ src/styles/media.js
@@ -2,7 +2,7 @@ export const mediaQuery = maxWidth => `
   @media (max-width: ${maxWidth}px)
 `;
 
-const media = {
+export default {
   xxlarge: mediaQuery(1920),
   xlarge: mediaQuery(1440),
   large: mediaQuery(1200),
@@ -11,5 +11,3 @@ const media = {
   xsmall: mediaQuery(375),
   custom: mediaQuery,
 };
-
-export default media;
---
+++ src/styles/palette.js
@@ -0,0 +1,27 @@
+import { css } from '@emotion/core';
+
+// TODO: Add colors
+export const palette = {};
+
+export default {
+  base: {
+    light: css`
+      color: #2b2b2b;
+      background: #fafafa;
+    `,
+    dark: css`
+      color: #fafafa;
+      background: #2b2b2b;
+    `,
+  },
+  primary: {
+    light: {
+      color: '',
+      background: '',
+    },
+    dark: {
+      color: '',
+      background: '',
+    },
+  },
+};
