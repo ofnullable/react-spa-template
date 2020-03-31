b58da06382163526fd819ffd361e1efdd42ced78
ofnullable <ofnullable@gmail.com>
2020-04-01 01:01:43 +0900

    :sparkles: Update error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,26 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
+import { default as TakenSVG } from '../assets/undraw_Taken.svg';
+import { palette } from '../styles/palette';
+import { useThemeContext } from '../contexts/ThemeContext';
+import media from '../styles/media';
 
-const ErrorPageStyle = css`
+const ErrorPageStyle = isLight => css`
   flex: 1;
   display: flex;
   flex-direction: column;
-  
+  align-items: center;
+  justify-content: center;
+
   h1 {
     color: red;
-    font-size: 5rem;
+    font-size: 3rem;
     font-weight: 600;
-    text-align: center;
+    color: ${isLight ? palette.red[6] : palette.red[5]};
+  }
+  img {
+    margin: 1rem auto;
+    max-width: 35%;
+  }
+  a {
+    color: ${isLight ? palette.blue[6] : palette.blue[4]};
+  }
+
+  ${media.medium} {
+    img {
+      margin: 2rem auto;
+      max-width: 70%;
+    }
+  }
+  ${media.small} {
+    h1 {
+      font-size: 2.5rem;
+    }
+    img {
+      margin: 2rem auto;
+      max-width: 80%;
+    }
   }
 `;
 
 const ErrorPage = () => {
-    return (
-        <div css={[ErrorPageStyle]}>
-            <h1>Page Not Found.</h1>
-        </div>
-    );
+  const { isLight } = useThemeContext();
+
+  console.log(TakenSVG);
+
+  return (
+    <div css={[ErrorPageStyle(isLight)]}>
+      <h1>Page Not Found.</h1>
+      <img src={TakenSVG} alt="" />
+      <Link to="/">go Home</Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
